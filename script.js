6038b1565332cd3841457d802c6ca4529a99ed29
yildizonurtb-create <yildizonurtb@gmail.com>
2026-07-07 02:33:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+const resim = document.querySelector('.app-ltr-7wh13m');
+const cizgi = document.querySelector('.app-ltr-5qxxfg');
 
+if (resim && cizgi) {
+    // 1. Resmi çizginin altına taşı
+    cizgi.after(resim);
+    
+    // 2. Resmin üstüne ve altına boşluk ekle (Sağ ve sol 0 kalır)
+    resim.style.margin = '15px 0'; 
+}
