2b3d613e3ebcacf8035713c1e16441097ecb5e58
yildizonurtb-create <yildizonurtb@gmail.com>
2026-07-07 17:37:05 +0300

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,10 @@
+// 1. Taşınacak olan banner elementini seçiyoruz
+const banner = document.querySelector('section[data-mj="widget-fixed-image-banner"]');
 
+// 2. Altına eklenecek olan footer içerik alanını seçiyoruz
+const footerContent = document.querySelector('div[data-mj="footer-content"]');
+
+// 3. Eğer her iki element de sayfada mevcutsa, banner'ı footer'ın hemen altına taşıyoruz
+if (banner && footerContent) {
+    footerContent.after(banner);
+}
