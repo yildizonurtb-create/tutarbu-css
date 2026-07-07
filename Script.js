2b552aa075eae557e4f3343d93915ff861a17f3d
yildizonurtb-create <yildizonurtb@gmail.com>
2026-07-07 17:41:48 +0300

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,9 +1,1 @@
-const moveInterval = setInterval(() => {
-    const banner = document.querySelector('section[data-mj="widget-fixed-image-banner"]');
-    const footer = document.querySelector('div[data-mj="footer-content"]');
 
-    if (banner && footer) {
-        footer.after(banner);
-        clearInterval(moveInterval); // Elementler taşındı, döngüyü güvenle kapat.
-    }
-}, 100); // Her 100 milisaniyede bir elementleri kontrol eder.
