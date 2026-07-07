34656d801ec0fe160b5379f35879b73d43f0005c
yildizonurtb-create <yildizonurtb@gmail.com>
2026-07-07 17:14:10 +0300

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,11 +1,19 @@
-const resim = document.querySelector('.app-ltr-7wh13m');
-const cizgi = document.querySelector('.app-ltr-5qxxfg');
+(function () {
+  function tasiResmi() {
+    const resim = document.querySelector('.app-ltr-7wh13m');
+    const cizgi = document.querySelector('.app-ltr-5qxxfg');
 
-if (resim && cizgi) {
-    // 1. Resmi çizginin altına taşı
-    cizgi.after(resim);
-    
-    // 2. Resmin üstüne ve altına boşluk ekle (Sağ ve sol 0 kalır)
-    resim.style.margin = '15px 0'; 
-}
+    if (resim && cizgi && resim.previousElementSibling !== cizgi) {
+      cizgi.after(resim);
+      resim.style.margin = '15px 0';
+    }
+  }
 
+  tasiResmi();
+
+  const bannerObserver = new MutationObserver(tasiResmi);
+  bannerObserver.observe(document.body, {
+    childList: true,
+    subtree: true
+  });
+})();
