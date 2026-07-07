41d66bffa9d449efc46aad2c8be5f34311b41032
yildizonurtb-create <yildizonurtb@gmail.com>
2026-07-07 17:30:43 +0300

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,19 +1,1 @@
-(function () {
-  function tasiResmi() {
-    const resim = document.querySelector('.app-ltr-7wh13m');
-    const cizgi = document.querySelector('.app-ltr-5qxxfg');
 
-    if (resim && cizgi && resim.previousElementSibling !== cizgi) {
-      cizgi.after(resim);
-      resim.style.margin = '15px 0';
-    }
-  }
-
-  tasiResmi();
-
-  const bannerObserver = new MutationObserver(tasiResmi);
-  bannerObserver.observe(document.body, {
-    childList: true,
-    subtree: true
-  });
-})();
